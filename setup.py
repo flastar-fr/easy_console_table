e774effc37d3dfab0692b70d43967bcec79168c1
flastar-fr <hgbequet@gmail.com>
2024-02-19 21:26:03 +0100

    Base Initialization

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easy-console-table",
+    version="0.0.1",
+    author="flastar",
+    url="",
+    description="Simple package to create console's tables !",
+    packages=find_packages(),
+    readme="README.md",
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
