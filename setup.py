52942996876d84c50a2cfa3810ca21bc13e70462
flastar-fr <hgbequet@gmail.com>
2024-02-19 21:34:17 +0100

    Base Initialization

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+
+with open("app/README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name="easy-console-table",
     version="0.0.1",
-    author="flastar",
-    url="",
-    description="Simple package to create console's tables !",
-    packages=find_packages(),
-    readme="README.md",
-    python_requires=">=3.10",
+    description="Simple package to create console table",
+    package_dir={"": "easy_console_table"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArjanCodes/2023-package",
+    author="ArjanCodes",
+    author_email="arjan@arjancodes.com",
+    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
 )
