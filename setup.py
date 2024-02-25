1f9d1ab5bf5ee9425c81143481672912be099465
flastar-fr <hgbequet@gmail.com>
2024-02-25 15:33:24 +0100

    Use of pyproject.toml

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from setuptools import find_packages, setup
+import setuptools
 
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(
-    name="easy-console-table",
-    version="0.0.3",
-    description="A simple package to create a table in console",
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/flastar-fr/easy_console_table",
-    author="flastar",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.10",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.10",
-)
+setuptools.setup()
